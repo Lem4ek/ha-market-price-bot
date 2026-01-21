eb44a40379ef1afa335f2beeb6dde3e956e7a134
Lem4ek <94479613+Lem4ek@users.noreply.github.com>
2026-01-21 23:14:15 +0300

    Update wb.py

--- bot/parsers/wb.py
+++ bot/parsers/wb.py
@@ -1,7 +1,12 @@
 import requests, re
 
 def parse_wb(url):
-    html = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}).text
+    html = requests.get(
+        url,
+        headers={"User-Agent": "Mozilla/5.0"}
+    ).text
+
     price = int(re.search(r'"salePriceU":(\d+)', html).group(1)) // 100
     title = re.search(r'"goodsName":"([^"]+)"', html).group(1)
+
     return {"title": title, "price": price, "url": url}
