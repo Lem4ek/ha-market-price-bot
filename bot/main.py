eb398af34d88b7990a1518cc461736a1c5c91aa6
Lem4ek <94479613+Lem4ek@users.noreply.github.com>
2026-01-21 23:08:09 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,7 @@
 import os
 from aiogram import Bot, Dispatcher, executor
 from handlers import register_handlers
+from scheduler import PriceScheduler
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = int(os.getenv("CHAT_ID"))
@@ -9,5 +10,8 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 register_handlers(dp, CHAT_ID)
+
+scheduler = PriceScheduler(bot, CHAT_ID)
+scheduler.start()
 
 executor.start_polling(dp)
