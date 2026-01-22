9168151918a11fa66a6f5145619e6c5744bd525e
Lem4ek <lem4ek@mail.ru>
2026-01-22 23:55:28 +0300

    я

--- addons/price-bot/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Передаём переменные окружения из config.yaml → .env или прямо в python
-# Можно использовать python-dotenv, но для простоты передаём через env
-
-exec python3 /app/bot.py
